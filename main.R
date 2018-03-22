e088a1ad4a892a5f1bac953256b8dda2d9945e8b
Chaitanya Reddy <chavalichaithanyachinna@gmail.com>
2018-03-22 14:57:07 +0530

    fix indentation

--- main.R
+++ main.R
@@ -2,13 +2,13 @@ library(shiny)
 library(plotly)
 
 # Reading the file
-raw_data = read.delim("data/GlobalLandTemperaturesByCountry.csv",sep=',')
+raw_data <- read.delim("data/GlobalLandTemperaturesByCountry.csv", sep = ",")
 
 # Missing values are greatest problem while visualization of data. So eliminating before going any further.
 # Finding the total number of missing values
 sum(is.na(raw_data))
 # Removing all the missing values
-countries_data = na.omit(raw_data)
+countries_data <- na.omit(raw_data)
 # checking whether all missing values are been removed or been
 sum(is.na(countries_data))
 
@@ -17,11 +17,11 @@ sum(is.na(countries_data))
 colnames(countries_data)
 
 # We need to create dropdown of countries. So we will first get all the countries from the data
-countries=unique(countries_data$Country)
+countries <- unique(countries_data$Country)
 
 # Coming on to shiny UI
 # Use a fluid Bootstrap layout
-ui=fluidPage(    
+ui <- fluidPage(
   # Give the page a title
   titlePanel("Temperature Visualizations"),
   # Creating Tabs
@@ -30,50 +30,64 @@ ui=fluidPage(
     tabPanel(
       title = "By Country (Line Chart)",
       # Dropdown of countries
-      sidebarLayout(sidebarPanel(selectInput("tab1_countries", "Countries:", 
-                                             choices=countries,
-                                             selected = "India"),
-                                 hr(),
-                                 helpText("Please select a country from the dropdown.")),
-                    mainPanel(
-                      # Chart Area
-                      plotlyOutput("tab1_plot_area")  
-                    ))
+      sidebarLayout(
+        sidebarPanel(
+          selectInput("tab1_countries", "Countries:",
+            choices = countries,
+            selected = "India"
+          ),
+          hr(),
+          helpText("Please select a country from the dropdown.")
+        ),
+        mainPanel(
+          # Chart Area
+          plotlyOutput("tab1_plot_area")
+        )
+      )
     ),
     # Tab 2
     tabPanel(
       title = "Two countries (Scatter Plot)",
       sidebarPanel(
-        selectInput("tab2_countryA", "Countries A:", 
-                    choices=countries,
-                    selected = "India"),
-        selectInput("tab2_countryB", "Countries B:", 
-                    choices=countries,
-                    selected = "Africa"),
+        selectInput("tab2_countryA", "Countries A:",
+          choices = countries,
+          selected = "India"
+        ),
+        selectInput("tab2_countryB", "Countries B:",
+          choices = countries,
+          selected = "Africa"
+        ),
         hr(),
-        helpText("Please select two countries from the dropdown")),
+        helpText("Please select two countries from the dropdown")
+      ),
       mainPanel(
         plotlyOutput("tab2_plot_area")
-      )),
+      )
+    ),
     # Tab 3
     tabPanel(
       title = "Average Temperature (Bar Chart)",
-      sidebarLayout(sidebarPanel(selectInput("tab3_countries", "Countries:", 
-                                             choices=countries,
-                                             selected = "India"),
-                                 hr(),
-                                 helpText("Please select a country from the dropdown.")),
-                    mainPanel(
-                      plotlyOutput("tab3_plot_area") 
-                    )))
-    
+      sidebarLayout(
+        sidebarPanel(
+          selectInput("tab3_countries", "Countries:",
+            choices = countries,
+            selected = "India"
+          ),
+          hr(),
+          helpText("Please select a country from the dropdown.")
+        ),
+        mainPanel(
+          plotlyOutput("tab3_plot_area")
+        )
+      )
+    )
   )
 )
 
 
 # Coming on to Shiny Backend
 # Define a server for the Shiny app
-server=function(input, output) {
+server <- function(input, output) {
   # Tab 1 processings
   # Custom font to display neatly
   custom_font <- list(
@@ -92,36 +106,35 @@ server=function(input, output) {
   # Filling the space with plot using plotly
   output$tab1_plot_area <- renderPlotly({
     # Extracting selected country data from whole data
-    tab1_extract_data = isolate(subset(countries_data,countries_data$Country==input$tab1_countries))
+    tab1_extract_data <- isolate(subset(countries_data, countries_data$Country == input$tab1_countries))
     # We only need two columns from it
-    date = tab1_extract_data$dt
-    avg_temperature = tab1_extract_data$AverageTemperature
-    data_frame <- data.frame(date,avg_temperature)
-    plot_ly(data_frame, x = ~date, y = ~avg_temperature, type = 'scatter', mode = 'lines')%>%
-      layout(title =input$tab1_countries, xaxis = x, yaxis = y, margin = 220)
+    date <- tab1_extract_data$dt
+    avg_temperature <- tab1_extract_data$AverageTemperature
+    data_frame <- data.frame(date, avg_temperature)
+    plot_ly(data_frame, x = ~ date, y = ~ avg_temperature, type = "scatter", mode = "lines") %>%
+      layout(title = input$tab1_countries, xaxis = x, yaxis = y, margin = 220)
   })
-  
+
   # Tab 2 processings
   # Extract 2 countries data
   layout_title <- reactive({
     paste(input$tab2_countryA, input$tab2_countryB, sep = " and ")
   })
   output$tab2_plot_area <- renderPlotly({
-    tab2_extract_data_countryA <- isolate(subset(countries_data,countries_data$Country==input$tab2_countryA))
-    tab2_extract_data_countryB <- isolate(subset(countries_data,countries_data$Country==input$tab2_countryB))
-    tab2_extract_data <- isolate(merge(tab2_extract_data_countryA , tab2_extract_data_countryB , all=TRUE))
-    plot_ly(data = tab2_extract_data, x = ~dt, y = ~AverageTemperature, color = ~Country, type="scatter", colors = "Set2")%>%
+    tab2_extract_data_countryA <- isolate(subset(countries_data, countries_data$Country == input$tab2_countryA))
+    tab2_extract_data_countryB <- isolate(subset(countries_data, countries_data$Country == input$tab2_countryB))
+    tab2_extract_data <- isolate(merge(tab2_extract_data_countryA, tab2_extract_data_countryB, all = TRUE))
+    plot_ly(data = tab2_extract_data, x = ~ dt, y = ~ AverageTemperature, color = ~ Country, type = "scatter", colors = "Set2") %>%
       layout(title = layout_title(), xaxis = x, yaxis = y, margin = 220)
   })
-  
+
   # Tab 3 Processings
   output$tab3_plot_area <- renderPlotly({
-    tab3_extract_data = isolate(subset(countries_data,countries_data$Country==input$tab3_countries))
-    plot_ly(y = ~tab3_extract_data$AverageTemperature, type="bar")%>%
+    tab3_extract_data <- isolate(subset(countries_data, countries_data$Country == input$tab3_countries))
+    plot_ly(y = ~ tab3_extract_data$AverageTemperature, type = "bar") %>%
       layout(title = input$tab3_countries, yaxis = y, margin = 220)
   })
-  
 }
 
-# Get going 
+# Get going
 shinyApp(ui = ui, server = server)
